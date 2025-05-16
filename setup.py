b72adddfedeb2dd7cabce949decf0960bc0a47d3
Douper <106928620+Douper2@users.noreply.github.com>
2025-05-16 23:31:01 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='dexonlineapi',
+    version='0.0.1',
+    author='Douper',
+    url='https://github.com/Douper2/dexonlineapi',
+    install_requires=['requests', 'bs4', 'html5lib']
+)
