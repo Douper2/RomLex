35f105a84a6c2c45d07a8a4492cd9ae0ca72dfc2
Douper <106928620+Douper2@users.noreply.github.com>
2025-05-17 17:55:26 +0300

    updated dexonlineapi.py
    added two new functions: getrandomword(); getwordofday()

--- dexonlineapi/dexonlineapi.py
+++ dexonlineapi/dexonlineapi.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import random
 
 
 # English: An API for the dexonline.ro website, a website for the DEX (The Romanian Explanatory Dictionary)
@@ -36,8 +37,8 @@ def getetymology(word):
         return "The etymology cannot be found. Check for typos"
 
     etym_text = etymology.get_text(separator=" ", strip=True)
-    clean_etym = re.sub(r"DEX\s*'?\d+", "", etym_text).strip() # removes the DEX and year text from the etymology
-    clean_etym = re.sub(r'\b(DN|DLRM|NODEX)\b', ' ', clean_etym) # removes the internal abbreviations
+    clean_etym = re.sub(r"DEX\s*'?\d+", "", etym_text).strip() # removes the DEX text and year text from the etymology
+    clean_etym = re.sub(r'\b(DN|DLRM|NODEX)\b', ' ', clean_etym) # removes the internal dexonline abbreviations
     return clean_etym
 
     match = re.search(r'From\s+(\w+)\.?\s+([\w\-]+)', etym_text)
@@ -47,3 +48,35 @@ def getetymology(word):
         return f'From {lang}: "{root}"'
 
     return f"Etymology: {etym_text}"
+
+def getrandomword(word=None):
+    url = "https://dexonline.ro/cuvinte-aleatorii"
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        return f"Error: {response.status_code}"
+
+    soup = BeautifulSoup(response.text, features="html5lib")
+    ranwords = soup.find_all("a", href=lambda x: x and x.startswith("/definitie/"))
+    words = [w.text.strip() for w in ranwords if w.text.strip()]
+    chosen_word = random.choice(words)
+
+    if not ranwords:
+        return "The random words cannot be found for some reason."
+
+    return chosen_word
+
+def getwordofday(word=None):
+    url = "https://dexonline.ro/cuvantul-zilei"
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        return f"Error: {response.status_code}"
+
+    soup = BeautifulSoup(response.text, features="html5lib")
+    wordday = soup.find("b")
+    wordofday = wordday.get_text().strip()
+    if not wordday:
+        return "The word of the day cannot be found for some reason."
+
+    return wordofday
